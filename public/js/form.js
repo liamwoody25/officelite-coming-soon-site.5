08aab0f229a1784f761fb9b07830c0c6e78b1e9b
liam woodward <liamwoodward20@outlook.com>
2025-12-10 13:01:18 +0000

    updating html and css and also adding the email input to javascript

--- public/js/form.js
+++ public/js/form.js
@@ -1,7 +1,12 @@
  const formToggle = document.querySelector('.package-toggle');
- const packageOptions = document.querySelector('.package-menu')
+ const submitBtn = document.querySelector('.form-btn');
+ const packageOptions = document.querySelector('.package-menu');
  const userEmail = document.getElementById('email-input');
+ const incorrectMessage = document.querySelector('.error-message');
 
+
+
+// this function is for the toggle for the select package
 function selectPackage(){
   if (formToggle.classList.toggle('.active')) {
     packageOptions.style.display = 'block'
@@ -11,7 +16,20 @@ function selectPackage(){
 }
 
 
+// this function is for when an email is incorrect
+function invalidEmail(){
+  if (userEmail.value !== '') {
+    userEmail.style.borderBottom = '2px solid #e74c3c'
+    incorrectMessage.style.display = 'block'
+  }
+}
+
+
 
  formToggle.addEventListener('click', function(){
   selectPackage()
+ })
+
+ submitBtn.addEventListener('click', function(){
+  invalidEmail()
  })
